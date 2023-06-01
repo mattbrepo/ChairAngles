0d20b5596b276b536d4b30ec892d93f3d9f82f94
Matteo <matteo.bertola77@gmail.com>
2023-06-01 10:09:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # ChairAngles
-A few calculations for a folding chair project
+A few calculations for a folding chair project.
+
+**Language: Python**
+
+**Start: 2023**
+
+## Why
+I found a [woodworking project](https://canadianwoodworking.com/project/folding-chair/) for a folding chair. The chair is composed by two pieces:
+
+![chair](/images/chair1.jpg)
+![chair](/images/chair2.jpg)
+
+I made a few calculations to adjust the angle between the two pieces, and wrote a python program to display a schematic of the chair.
+
+## Calculations
+To explain the calculations it's necessary to define a few variables. First of all, the name of the four key blocks involved in red:
+
+![chair](/images/drawing2.jpg)
+
+Each block is characterized by a key length with the same name:
+
+![chair](/images/drawing3.jpg)
+
+Other relevant measurements:
+
+![chair](/images/drawing4.jpg)
+
+Keeping in mind that the block l and m have the same depth h, and the blocks b, c, d have the same dimension e, f.
+
+We define also a few variables zooming in where the block _l_ and _m_ meet:
+
+![chair](/images/drawing5.jpg)
+
+$$t = 2 \cdot f + h$$
+
+$$u = m - d - 2 \cdot e - b$$
+
+$$u = w + s$$
+
+### Finding &theta;
+&theta; can be defined with two formulas:
+
+$$cos \theta = \frac{h}{s}$$
+
+$$tan \theta = \frac{w}{t} = \frac{u - s}{t}$$
+
+To solve for &theta; we must remember a property of the tangent:
+
+$$tan \theta = \frac{\sqrt{1-cos^2 \theta}}{\cos \theta}$$
+
+With this we can write:
+
+$$\frac{\sqrt{1-cos^2 \theta}}{\cos \theta} = \frac{1}{t} \cdot (u - \frac{h}{cos \theta})$$
+
+$$\sqrt{1-cos^2 \theta} = \frac{u}{t} \cdot \cos \theta - \frac{h}{t}$$
+
+$$1-cos^2 \theta = \frac{u^2}{t^2} \cdot \cos^2 \theta + \frac{h^2}{t^2} - 2 \cdot \frac{u}{t} \cdot \cos \theta \cdot \frac{h}{t}$$
+
+$$\frac{u^2}{t^2} \cdot \cos^2 \theta + cos^2 \theta - 2 \cdot \frac{u \cdot h}{t^2} \cdot \cos \theta + \frac{h^2}{t^2} - 1 = 0$$
+
+$$(\frac{u^2}{t^2} + 1) \cdot \cos^2 \theta - 2 \cdot \frac{u \cdot h}{t^2} \cdot \cos \theta + \frac{h^2}{t^2} - 1 = 0$$
+
+We can now solve this quadratic equation for _cos_ &theta;:
+
+$$\cos \theta = \frac{2 \cdot \frac{u \cdot h}{t^2} \pm \sqrt{4 \cdot \frac{u^2 \cdot h^2}{t^4} - 4 \cdot (\frac{u^2}{t^2} + 1) \cdot (\frac{h^2}{t^2} - 1)}}{2 \cdot (\frac{u^2}{t^2} + 1)}$$
+
+### Finding &gamma;, _m1_ and _l1_
+With this we can calculate &gamma;, m1 and l1:
+
+$$\gamma = \theta + 90°$$
+
+$$m_1 = b + e + (f + h) \cdot tan \theta$$
+
+$$l_1 = l - e - c - \frac{h}{\cos \theta} + h \cdot \tan \theta$$
+
+### Finding &alpha;, &beta; and _a_
+Finally, we can calculate &alpha; and &beta; by first calculating _a_ using the [law of cosines](https://en.wikipedia.org/wiki/Law_of_cosines):
+
+$$a = \sqrt{l_1^2 + m_1^2 - 2 \cdot l_1 \cdot m_1 \cdot \cos \gamma}$$
+
+$$\alpha = \arcsin \frac{m_1 \cdot \sin \gamma}{a}$$
+
+$$\beta = 180° - \gamma - \alpha$$
---
+++ chair.py
@@ -0,0 +1,164 @@
+import turtle
+import math
+
+def mycos(degree):
+  return math.cos(math.radians(degree))
+
+def mysin(degree):
+  return math.sin(math.radians(degree))
+
+def calculate(l, m, h, b, c, d, e, f):
+  t = h + 2 * f
+  u = m - d - 2 * e - b
+  
+  snddeg_a = (u**2 / t**2 + 1)
+  snddeg_b = -2*u*h / t**2
+  snddeg_c = h**2 / t**2 - 1
+  snddeg1 = (-snddeg_b + math.sqrt(snddeg_b**2 - 4 * snddeg_a * snddeg_c)) / (2 * snddeg_a)
+  snddeg2 = (-snddeg_b - math.sqrt(snddeg_b**2 - 4 * snddeg_a * snddeg_c)) / (2 * snddeg_a)
+  
+  theta = math.acos(snddeg1)
+  theta_deg = math.degrees(theta)
+  theta_deg2 = math.degrees(math.acos(snddeg2))
+  if theta_deg > theta_deg2:
+    theta_deg = theta_deg # prodebug: it may be worth investigating
+  
+  gamma = theta + math.radians(90)
+  gamma_deg = math.degrees(gamma)
+  m1 = b + e + (f + h) * math.tan(theta)
+  l1 = l - e - c - h / math.cos(theta) + h * math.tan(theta)
+  a = math.sqrt(l1 ** 2 + m1 ** 2 - 2 * l1 * m1 * math.cos(gamma))
+  alpha = math.asin((m1 * math.sin(gamma)) / a)
+  
+  alpha_deg = math.degrees(alpha)
+  beta_deg = 180 - gamma_deg - alpha_deg
+  #if beta > 90:
+  #  beta = 360 - beta
+  return (alpha_deg, beta_deg, a, l1, m1)
+
+def drawMeasurement(t, points, msg, font, msgPoint):
+  t.pencolor('red')
+  drawLines(t, points)
+  t.goto(msgPoint)
+  t.write(msg, font = font)
+  t.pencolor('black')
+  
+def drawLines(t, points):
+  t.penup()
+  for point in points:
+    t.goto(point)
+    t.pendown()
+  t.penup()
+    
+def drawIt(l, m, h, alpha, beta, a, b, c, d, e, f, l1, m1):
+  turtle.bgcolor('white')
+
+  t = turtle.Turtle()
+  t.pensize(3)
+  t.speed(10)
+  font = ('Arial', 12, 'normal')
+  #t.forward(100)
+  #t.right(90)
+  #point1 = (50, 100)
+  #point2 = (150, 200)
+  #points = [point1, point2]
+  
+  # the origin is the lower cross point between block l and block m
+  O = (0, -100)
+  antibeta = 180 - 90 - beta
+  antialpha = 180 - 90 - alpha
+
+  # ground
+  t.pencolor('green')
+  points = []
+  points.append((O[0] - l1 * mycos(alpha) - 150, O[1] - l1 * mysin(alpha)))
+  points.append((O[0] + m1 * mycos(beta) + 150, O[1] - m1 * mysin(beta)))
+  drawLines(t, points)
+  t.pencolor('black')
+  
+  # key info
+  t.goto((0, +200))
+  t.write('alpha: ' + str(round(alpha, 1)), font = font)
+  t.goto((0, +170))
+  t.write('beta: ' + str(round(beta, 1)), font = font)
+  
+  # block m
+  points = []
+  points.append(O)
+  points.append((points[-1][0] + m1 * mycos(beta), points[-1][1] - m1 * mysin(beta))) # go to the previous origin
+  points.append((points[-1][0] - m * mycos(beta), points[-1][1] + m * mysin(beta)))
+  pointM_LeftUp = points[-1]
+  points.append((points[-1][0] + h * mycos(antibeta), points[-1][1] + h * mysin(antibeta)))
+  points.append((points[-1][0] + m * mycos(beta), points[-1][1] - m * mysin(beta)))
+  pointM_RightDown = points[-1]
+  points.append(points[1])
+  drawLines(t, points)
+  
+  if False:
+    points = []
+    points.append((O[0], O[1] + 500))
+    points.append((points[-1][0] + m * mycos(beta), points[-1][1] - m * mysin(beta)))
+    drawMeasurement(t, points, 'm', font, (points[0][0] + m/2 * mycos(beta), points[0][1] - m/2 * mysin(beta)))
+
+  # block b
+  points = []
+  #points.append((O[0] + (h - b) * mycos(antibeta), O[1] + (h - b) * mysin(antibeta)))
+  points.append((pointM_RightDown[0] - b * mycos(beta), pointM_RightDown[1] + b * mysin(beta)))
+  points.append((points[-1][0] + f * mycos(antibeta), points[-1][1] + f * mysin(antibeta)))
+  points.append((points[-1][0] - e * mycos(beta), points[-1][1] + e * mysin(beta)))
+  points.append((points[-1][0] - f * mycos(antibeta), points[-1][1] - f * mysin(antibeta)))
+  drawLines(t, points)
+
+  # block d
+  points = []
+  points.append((pointM_LeftUp[0] + (d + e) * mycos(beta), pointM_LeftUp[1] - (d + e) * mysin(beta)))
+  points.append((points[-1][0] - f * mycos(antibeta), points[-1][1] - f * mysin(antibeta)))
+  points.append((points[-1][0] - e * mycos(beta), points[-1][1] + e * mysin(beta)))
+  points.append((points[-1][0] + f * mycos(antibeta), points[-1][1] + f * mysin(antibeta)))
+  drawLines(t, points)
+
+  # block l
+  points = []
+  points.append(O)
+  points.append((points[-1][0] - l1 * mycos(alpha), points[-1][1] - l1 * mysin(alpha)))
+  points.append((points[-1][0] + l * mycos(alpha), points[-1][1] + l * mysin(alpha)))
+  points.append((points[-1][0] - h * mycos(antialpha), points[-1][1] + h * mysin(antialpha)))
+  pointL_RightUp = points[-1]
+  points.append((points[-1][0] - l * mycos(alpha), points[-1][1] - l * mysin(alpha)))
+  pointL_LeftUp = points[-1]
+  points.append(points[1])
+  drawLines(t, points)
+  
+  # block c
+  points = []
+  points.append((pointL_RightUp[0] - (c + e) * mycos(alpha), pointL_RightUp[1] - (c + e) * mysin(alpha)))
+  points.append((points[-1][0] - f * mycos(antialpha), points[-1][1] + f * mysin(antialpha)))
+  points.append((points[-1][0] + e * mycos(alpha), points[-1][1] + e * mysin(alpha)))
+  points.append((points[-1][0] + f * mycos(antialpha), points[-1][1] - f * mysin(antialpha)))
+  drawLines(t, points)
+  
+  t.hideturtle()
+  turtle.Screen().exitonclick()  
+
+#
+# Main
+#
+
+if __name__ == "__main__":
+  l = 400
+  m = 400
+  h = 30
+  e = 20
+  f = 20
+
+  b = 25
+  gap_block_bd = 100
+  c = 100
+  d = m - 2 * e - gap_block_bd
+  
+  (alpha, beta, a, l1, m1) = calculate(l, m, h, b, c, d, e, f)
+  
+  drawIt(l, m, h, alpha, beta, a, b, c, d, e, f, l1, m1)
+  
+  
+  
